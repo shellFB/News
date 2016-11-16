664bffc7c53ff45c6d0c36ab24cc4bfc2d1d8f02
fb <2607630036@qq.com>
2016-11-16 17:17:56 +0800

    splash页面动画的实现

--- app/src/main/java/com/dasheng/news/SplashActivity.java
+++ app/src/main/java/com/dasheng/news/SplashActivity.java
@@ -1,10 +1,21 @@
 package com.dasheng.news;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.dasheng.news.utils.WindowTittle;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * @author shell
@@ -12,23 +23,42 @@ import com.dasheng.news.utils.WindowTittle;
  * @desc
  * @since 2016/11/16 0016 15:25
  */
-public class SplashActivity extends AppCompatActivity{
+public class SplashActivity extends AppCompatActivity {
+    boolean isShowingRubberEffect = false;
+    @InjectView(R.id.logo_outer_iv)
+    ImageView logoOuterIv;
+    @InjectView(R.id.logo_inner_iv)
+    ImageView logoInnerIv;
+    @InjectView(R.id.app_name_tv)
+    TextView appNameTv;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         WindowTittle.setWindowTittle(this);
-        overridePendingTransition(R.anim.zoomin,0);//页面加载动画
+        overridePendingTransition(R.anim.zoomin, 0);//页面加载动画
         setContentView(R.layout.activity_splash);
+        ButterKnife.inject(this);
         initAnimation();
     }
 
     private void initAnimation() {
         startInnerAnimation();
-        startLogoOuterAndAppName();
+        startShowAppName();
+        startLogoOuter();
     }
-    private void startLogoOuterAndAppName() {
+
+    private void startShowAppName() {
+        Animation animation=AnimationUtils.loadAnimation(this,R.anim.anim_splash_appname);
+        appNameTv.startAnimation(animation);
+    }
+
+    private void startLogoOuter() {
+
     }
 
     private void startInnerAnimation() {
+        Animation animation= AnimationUtils.loadAnimation(this,R.anim.anim_top_in);
+        logoInnerIv.startAnimation(animation);
     }
 }
