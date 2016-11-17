38934fedc3fff033b56a8e5d2ecdcd4e841a6899
fb <2607630036@qq.com>
2016-11-17 10:09:51 +0800

    使用mvp模式开发

---
+++ app/src/main/java/com/dasheng/news/MyApplication.java
@@ -0,0 +1,13 @@
+package com.dasheng.news;
+
+import android.app.Application;
+
+/**
+ * @author shell
+ * @version 1.0
+ * @desc
+ * @since 2016/11/17 0017 9:23
+ */
+public class MyApplication extends Application {
+
+}
