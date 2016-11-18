3df9168e2f2ab440790311a39b591343fe603779
fb <2607630036@qq.com>
2016-11-18 09:10:02 +0800

    ewwew

--- app/src/main/java/com/dasheng/news/MyApplication.java
+++ app/src/main/java/com/dasheng/news/MyApplication.java
@@ -28,7 +28,7 @@ import de.greenrobot.dao.query.QueryBuilder;
 /**
  * @author shell
  * @version 1.0
- * @desc ee
+ * @desc
  * @since 2016/11/17 0017 9:23
  */
 public class MyApplication extends Application {
